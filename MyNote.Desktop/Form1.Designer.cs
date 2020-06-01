89b3cfaf91118aa35c9d51fff527dfc5adcca398
okancan karadağ <karadagokancan@gmail.com>
2020-06-01 21:43:47 +0300

    FormApp

---
+++ MyNote.Desktop/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace MyNote.Desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                client.Dispose();
+            }
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnListNotes = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstNotes = new System.Windows.Forms.ListBox();
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UserName";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(12, 70);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(237, 26);
+            this.txtUserName.TabIndex = 1;
+            this.txtUserName.Text = "karadagokancan@gmail.com";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(255, 70);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(217, 26);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.Text = "Password1.";
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // btnListNotes
+            // 
+            this.btnListNotes.Location = new System.Drawing.Point(478, 70);
+            this.btnListNotes.Name = "btnListNotes";
+            this.btnListNotes.Size = new System.Drawing.Size(92, 26);
+            this.btnListNotes.TabIndex = 3;
+            this.btnListNotes.Text = "List Notes";
+            this.btnListNotes.UseVisualStyleBackColor = true;
+            this.btnListNotes.Click += new System.EventHandler(this.btnListNotes_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // lstNotes
+            // 
+            this.lstNotes.FormattingEnabled = true;
+            this.lstNotes.ItemHeight = 20;
+            this.lstNotes.Location = new System.Drawing.Point(12, 138);
+            this.lstNotes.Name = "lstNotes";
+            this.lstNotes.Size = new System.Drawing.Size(237, 204);
+            this.lstNotes.TabIndex = 7;
+            this.lstNotes.SelectedIndexChanged += new System.EventHandler(this.lstNotes_SelectedIndexChanged);
+            // 
+            // txtNote
+            // 
+            this.txtNote.Location = new System.Drawing.Point(255, 138);
+            this.txtNote.Multiline = true;
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(315, 204);
+            this.txtNote.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Titles";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(251, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Content";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 400);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNote);
+            this.Controls.Add(this.lstNotes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnListNotes);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "MyNote Desktop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnListNotes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstNotes;
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
